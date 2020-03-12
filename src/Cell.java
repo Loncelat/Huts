5f10cb00220cf75535075b98831c60126361e785
Gijsdeman <43375368+Gijsdeman@users.noreply.github.com>
2020-03-12 19:01:26 +0100

    Yeet

--- src/Cell.java
+++ src/Cell.java
@@ -1,2 +1,11 @@
 public class Cell {
+
+    public boolean isManager;
+    public Manager man;
+    public Developer dev;
+
+    Cell(boolean isManager) {
+        this.isManager = isManager;
+    }
+
 }
