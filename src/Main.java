b9ac2392bfb18c5b5ef7b5a01469284054bcc3b2
justsamuel <samuel.oosterholt20001004@gmail.com>
2020-03-12 18:19:47 +0100

    GIT GUD

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        // Input input = new Input();
+        // Params params = input.getParams();
+        // Solver solver = new Solver();
+        // Solution solution =  solver.getSolution();
+    }
+
+}
