87441eab81f678894593f832a9ce63ffb9561647
Gijsdeman <43375368+Gijsdeman@users.noreply.github.com>
2020-03-12 19:02:28 +0100

    Yeet3.13

--- src/Grid.java
+++ src/Grid.java
@@ -3,3 +3,16 @@ public class Grid {
 
 
 }
+
+
+class Cell {
+
+    public boolean isManager;
+    public Manager man;
+    public Developer dev;
+
+    Cell(boolean isManager) {
+        this.isManager = isManager;
+    }
+
+}
