475eb42079e6f86f0726b332e6522b04d47a88f6
Martijn Leus <m.h.m.leus@student.tue.nl>
2020-03-12 18:34:00 +0100

    fhauuvuia89

--- src/Manager.java
+++ src/Manager.java
@@ -1,6 +1,9 @@
+import java.util.*;
 public class Manager {
     int company;
     int bonus;
+    int x = -1;
+    int y = -1;
 
     public Manager(String company, int bonus) {
         this.company = company.hashCode();
