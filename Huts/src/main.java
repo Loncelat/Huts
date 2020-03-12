ccc155a11fede1487527338a5ee01d26ac7d678d
Gijsdeman <43375368+Gijsdeman@users.noreply.github.com>
2020-03-12 18:15:56 +0100

    Hello world!

---
+++ Huts/src/main.java
@@ -0,0 +1,12 @@
+class Main {
+
+    void readInput() {
+        System.out.println("begin!");
+    }
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        main.readInput();
+    }
+
+}
